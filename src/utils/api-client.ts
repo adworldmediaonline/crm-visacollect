8168288a051efdd54d08d458c5123db4f838e177
fastestsunil <sunil141292@gmail.com>
2025-04-04 08:47:51 +0530

    refactor: Update API client configuration and standardize quote usage
    - Changed import statements to use single quotes for consistency.
    - Updated the base URL to the production endpoint.
    - Simplified request and response interceptor syntax for improved readability.
    - Standardized token handling in request interceptor.

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-// const BASE_URL = "https://visa-backend-v2.vercel.app/api/v1/";
-const BASE_URL = "http://localhost:8090/api/v1/";
+const BASE_URL = 'https://visa-backend-v2.vercel.app/api/v1/';
+// const BASE_URL = "http://localhost:8090/api/v1/";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
   },
   timeout: 10000,
 });
 
 apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+  config => {
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     } else {
@@ -21,16 +21,16 @@ apiClient.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
   }
 );
 
 apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  response => response,
+  error => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
+      localStorage.removeItem('token');
       localStorage.clear();
       delete apiClient.defaults.headers.common.Authorization;
     }
